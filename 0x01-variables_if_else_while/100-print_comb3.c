c1b1eadab8c7911707763f0332bd002e1c3c0c45
kamau95 <linuskamau586@gmail.com>
2023-10-07 02:07:18 +0300

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - entry point.
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int d, p;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+	if (p != d)
+	{
+	putchar(d);
+	putchar(p);
+	if (d == '8' && p == '9)
+	continue;
+	putchar(', ');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
