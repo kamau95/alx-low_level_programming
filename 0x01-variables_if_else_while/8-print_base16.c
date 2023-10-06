b2719a6654415bf49407edacc9c034b4a07579d1
kamau95 <linuskamau586@gmail.com>
2023-10-07 01:21:47 +0300

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - main entry
+ *
+ *Return: always 0(success)
+ */
+int main(void)
+{
+	int c;
+	char b;
+
+	for (c = 0 ; c < 10 ; c++)
+		putchar(a + '0');
+	for (b = 'a' ; b <= 'f' ; j++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
