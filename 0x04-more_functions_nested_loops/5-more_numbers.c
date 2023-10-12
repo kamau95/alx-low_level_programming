7015524bc8b34f731b9bbbeca6ac28e04784e6d2
kamau95 <linuskamau586@gmail.com>
2023-10-12 13:55:31 -0700

    num

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints number ten times
+ */
+void more_numbers(void)
+{
+	int i, num;
+
+for (i = 0; i < 10; i++)
+{
+for (num = 0; num <= 14; num++)
+{
+_putchar(num + '0');
+
+}
+_putchar('\n');
+
+}
+}
