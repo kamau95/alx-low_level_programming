110cc2184d8b1d3eac65803d6777f4447aa06424
kamau95 <linuskamau586@gmail.com>
2023-10-12 11:40:03 -0700

    print no

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - prints from 0-9
+ *
+ */
+void print_numbers(void)
+{
+	char digit;
+
+for (digit = '0'; digit <= '9'; digit++)
+{
+	_putchar(digit)
+		;
+}
+	_putchar('\n');
+
+}
+
