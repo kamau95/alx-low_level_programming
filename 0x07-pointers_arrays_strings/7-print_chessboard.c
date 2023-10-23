8500f3da402bd7ae376a5ee97e41c44a222bb68d
kamau95 <linuskamau586@gmail.com>
2023-10-23 14:38:47 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,24 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *print_chessboard - prints a chessboard
- *@a: The chessboard to be printed
- *
- *Return: nothing
- */
-
+* print_chessboard - prints chessboard
+* @a: matrix
+*/
 void print_chessboard(char (*a)[8])
 {
+int i, j;
 
-	int indx1, indx2;
-
-	for (indx1 = 0; a[indx1][7]; indx1++)
-	{
-		for (indx2 = 0; indx2 < 8; indx2++)
-
-			_putchar(a[indx1][indx2]);
-
-		_putchar('\n');
-
-	}
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(*(*(i + a) + j));
+}
+_putchar('\n');
+}
 }
