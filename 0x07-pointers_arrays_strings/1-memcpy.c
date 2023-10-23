d8c6afb67f07f885a110e99b67408267a65a4d4f
kamau95 <linuskamau586@gmail.com>
2023-10-23 11:54:12 -0700

    cp mem area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,10 +7,9 @@
  * Return: pointer to destination string
  *
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *csrc = (char *)src;
-	char *cdest = (char *)dest;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 {
