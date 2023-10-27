95e19ceb24e4b0564b3f4cda348e3a3d114485fb
kamau95 <linuskamau586@gmail.com>
2023-10-27 12:53:42 -0700

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * return: (0) if successful
+ * @argc: numbers of things
+ * @argv:array of strings
+ * Return: 0 if success
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
